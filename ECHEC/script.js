1da5d9bdee8c7a1ed2306d168c197453a2b2cf5c
Gaby-GH <161126492+Gaby-GH@users.noreply.github.com>
2024-06-02 14:08:42 +0200

    Add files via upload

---
+++ ECHEC/script.js
@@ -0,0 +1,388 @@
+// WHITE
+
+let WRook1 = {
+    color: "W",
+    type: "rook",
+    position: [1, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_tour.svg",
+}
+
+let WRook2 = {
+    color: "W",
+    type: "rook",
+    position: [8, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_tour.svg",
+}  
+
+let WPony1 = {
+    color: "W",
+    type: "pony",
+    position: [2, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_poney.svg",
+}
+
+let WPony2 = {
+    color: "W",
+    type: "pony",
+    position: [7, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_poney.svg",
+}
+
+let WBishop1 = {
+    color: "W",
+    type: "bishop",
+    position: [3, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_fou.svg",
+}
+
+let WBishop2 = {
+    color: "W",
+    type: "bishop",
+    position: [6, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_fou.svg",
+}
+
+let WQueen = {
+    color: "W",
+    type: "queen",
+    position: [4, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_dame.svg",
+}
+
+let WKing = {
+    color: "W",
+    type: "king",
+    position: [5, 1],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_king.svg",
+}
+
+let WPawn1 = {
+    color: "W",
+    type: "pawn",
+    position: [1, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn2 = {
+    color: "W",
+    type: "pawn",
+    position: [2, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+let WPawn3 = {
+    color: "W",
+    type: "pawn",
+    position: [3, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn4 = {
+    color: "W",
+    type: "pawn",
+    position: [4, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn5 = {
+    color: "W",
+    type: "pawn",
+    position: [5, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn6 = {
+    color: "W",
+    type: "pawn",
+    position: [6, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn7 = {
+    color: "W",
+    type: "pawn",
+    position: [7, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+let WPawn8 = {
+    color: "W",
+    type: "pawn",
+    position: [8, 2],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/white_pion.svg",
+}
+
+// BLACK
+
+let BRook1 = {
+    color: "B",
+    type: "rook",
+    position: [1, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_tour.svg",
+}
+
+let BRook2 = {
+    color: "B",
+    type: "rook",
+    position: [8, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_tour.svg",
+}  
+
+let BPony1 = {
+    color: "B",
+    type: "pony",
+    position: [2, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_poney.svg",
+}
+
+let BPony2 = {
+    color: "B",
+    type: "pony",
+    position: [7, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_poney.svg",
+}
+
+let BBishop1 = {
+    color: "B",
+    type: "bishop",
+    position: [3, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_fou.svg",
+}
+
+let BBishop2 = {
+    color: "B",
+    type: "bishop",
+    position: [6, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_fou.svg",
+}
+
+let BQueen = {
+    color: "B",
+    type: "queen",
+    position: [4, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_dame.svg",
+}
+
+let BKing = {
+    color: "B",
+    type: "king",
+    position: [5, 8],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_king.svg",
+}
+
+let BPawn1 = {
+    color: "B",
+    type: "pawn",
+    position: [1, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn2 = {
+    color: "B",
+    type: "pawn",
+    position: [2, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+let BPawn3 = {
+    color: "B",
+    type: "pawn",
+    position: [3, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn4 = {
+    color: "B",
+    type: "pawn",
+    position: [4, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn5 = {
+    color: "B",
+    type: "pawn",
+    position: [5, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn6 = {
+    color: "B",
+    type: "pawn",
+    position: [6, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn7 = {
+    color: "B",
+    type: "pawn",
+    position: [7, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+let BPawn8 = {
+    color: "B",
+    type: "pawn",
+    position: [8, 7],
+    AlreadyMoved: false,
+    Moves: [],
+    chemin: "image/black_pion.svg",
+}
+
+
+let a = [WRook1, WPawn1, "x", "x", "x", "x", BPawn1, BRook1]
+let b = [WPony1, WPawn2, "x", "x", "x", "x", BPawn2, BPony1]                              
+let c = [WBishop1, WPawn3, "x", "x", "x", "x", BPawn3, BBishop1]
+let d = [WQueen, WPawn4, "x", "x", "x", "x", BPawn4, BQueen]
+let e = [WKing, WPawn5, "x", "x", "x", "x", BPawn5, BKing]
+let f = [WBishop2, WPawn6, "x", "x", "x", "x", BPawn6, BBishop2]
+let g = [WPony2, WPawn7, "x", "x", "x", "x", BPawn7, BPony2]
+let h = [WRook2, WPawn8, "x", "x", "x", "x", BPawn8, BRook2]
+let board = [a, b, c, d, e, f, g, h]
+
+function Init(){
+    for (let i=1; i<=8; i++){
+        for (let j=1; j<=8; j++){
+            let aff = document.getElementById(`${i}${j}`)
+            if (board[i - 1][j - 1] != "x"){
+                aff.src = board[i - 1][j - 1].chemin
+            }
+        }
+    }
+}
+
+Init()
+
+// Fonction Jeu
+
+Moves = []
+function ReturnId(id){
+    Moves.push(id)
+    Play()
+}
+
+let position = []
+let turn = "W"
+function Play(){
+    if (Moves.length == 1){
+        if (board[Moves[0][0] - 1][Moves[0][1] - 1] != "x"){
+            if (board[Moves[0][0] - 1][Moves[0][1] - 1].type = ) // CONTINUER ICI
+                                                                 // ADAPTER LE MOVE EN FONCTION DU
+                                                                 // TYPE DU PION ---> BOSSER SUR FONCTIONS PIONS
+        // PAS OUBLIER --> CALCULER SI ECHEC ETC ...
+
+
+        }else{
+            Moves = []
+        }
+
+    }else if(Moves.length == 2){
+        if (board[Moves[1][0] - 1][Moves[1][1] - 1] == "x"){
+            board[Moves[1][0] - 1][Moves[1][1] - 1] = board[Moves[0][0] - 1][Moves[0][1] - 1]
+            board[Moves[0][0] - 1][Moves[0][1] - 1] = "x"
+            console.log(board)
+
+        }else{
+            // Corriger ici --> si deuxieme touche = pion ^m couleur alors Moves[0] = ce nouveau pion
+            Moves = []
+            //Moves.push(board[Moves[1][0] - 1][Moves[1][1] - 1].position)
+        }
+
+        Moves = []
+
+        Update()
+    }
+}
+
+
+
+
+
+
+// fonction de calcul des pieces (begin)
+
+function MovePawn(pawn){
+    if (pawn.color == turn){
+        if (pawn.color == "W"){
+            pawn.position[0]
+            pawn.position[1]
+        }
+    }
+}
+
+MovePawn(board[0][1])
+
+// fonction de calcul des pieces (end)
+
+
+function Update(){
+    for (let i=1; i<=8; i++){
+        for (let j=1; j<=8; j++){
+            let aff = document.getElementById(`${i}${j}`)
+            if (board[i - 1][j - 1] != "x"){
+                aff.src = board[i - 1][j - 1].chemin
+            }else{
+                aff.src = ""
+            }
+        }
+    }
+}
